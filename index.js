cb29321c0ca3c90768dbc84b580f6b5ce0560e36
Abdul Muiz <113927276+TorqueKill@users.noreply.github.com>
2022-12-23 23:20:47 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const express = require('express')
-const Gun = require('gun');
+import express from 'express';
+import Gun from 'gun';
 const app = express()
 const port = 3030
 app.use(Gun.serve);
 
-const server = app.listen(process.env || port, () => {
-  console.log(`PORT : ${process.env || port}`)
+const server = app.listen(port, () => {
+  console.log(`PORT : ${port}`)
 })
 
 Gun({ web: server });
