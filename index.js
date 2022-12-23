5c9c0d749c11504283d9f2b876fda7a699697dec
Abdul Muiz <113927276+TorqueKill@users.noreply.github.com>
2022-12-23 23:35:08 +0500

    Update index.js

--- index.js
+++ index.js
@@ -2,15 +2,19 @@ import express from 'express';
 import Gun from 'gun';
 const app = express()
 const port = 3030
+
+app.use(json())
+
+app.use("/",(req,res,next)=>{
+  res.json({message:"server OK"})
+  next()
+})
+
 app.use(Gun.serve);
 
 const server = app.listen(port, () => {
   console.log(`PORT : ${port}`)
 })
 
-app.use("/",(req,res,next)=>{
-    res.json({message:"server OK"})
-    next()
-})
 
 Gun({ web: server });
