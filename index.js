a693371c855b0ba79baface8a7fef7cca6380a4d
Abdul Muiz <113927276+TorqueKill@users.noreply.github.com>
2022-12-23 23:39:38 +0500

    Update index.js

--- index.js
+++ index.js
@@ -5,12 +5,6 @@ const app = express()
 const port = 3030
 
 app.use(cors())
-app.use(json())
-
-app.use("/",(req,res,next)=>{
-  res.json({message:"server OK"})
-  next()
-})
 
 app.use(Gun.serve);
 
