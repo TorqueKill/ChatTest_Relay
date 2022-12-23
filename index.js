7260bb11c15cbcaa942c54259045c24de9085ff5
Abdul Muiz <113927276+TorqueKill@users.noreply.github.com>
2022-12-23 23:37:18 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-import express from 'express';
+import express, { json } from 'express';
 import Gun from 'gun';
+import cors from 'cors'
 const app = express()
 const port = 3030
 
+app.use(cors())
 app.use(json())
 
 app.use("/",(req,res,next)=>{
